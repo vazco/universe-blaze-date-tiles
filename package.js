35fb4563c7cea2b93855399ba328cf55816b0698
cristo-rabani <cristo.rabani@gmail.com>
2016-06-21 17:06:47 +0200

    up and fix

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
     name: 'universe:blaze-date-tiles',
-    version: '1.0.0',
+    version: '1.0.1',
     // Brief, one-line summary of the package.
     summary: 'Date tiles picker works as a standalone and as a autoform input',
     // URL to the Git repository containing the source code for this package.
-    git: '',
+    git: 'https://github.com/vazco/universe-blaze-date-tiles',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
